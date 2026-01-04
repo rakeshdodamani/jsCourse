95978fd84c75b3c046ab0f3307cd76b1aa1cfd4e
Rakesh Dodamani <111017434+rakeshdodamani@users.noreply.github.com>
2026-01-04 17:37:16 +0000

    Arrow function complete

---
+++ 03_basics/03_Arrow.js
@@ -0,0 +1,42 @@
+const user = {
+    username: "rakesh",
+    price: 999,
+    welcomeMessage: function () {
+        console.log(`${this.username}, welcome to website`);
+        console.log(this); 
+    }
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+// console.log(this);
+
+// function chai() {
+//     let username= 'rakesh'
+//     console.log(this.username);
+// }
+// chai()
+
+// const chai = function () {
+//     let username= 'rakesh'
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = () => {
+//     let username= 'rakesh'
+//     console.log(this);
+// }
+
+// chai()
+
+// const addTwo = (num1, num2) => num1 + num2
+// const addTwo = (num1, num2) => (num1 + num2)
+const addTwo = (num1, num2) => ({username: 'rakesh'})
+console.log(addTwo(3, 4))
+
+const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
