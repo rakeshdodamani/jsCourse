d448ed7a8e612b9f121ca46a051380258cf95f46
Rakesh Dodamani <111017434+rakeshdodamani@users.noreply.github.com>
2026-01-04 04:15:07 +0000

    variable complete

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,7 +2,19 @@ const accountId = 144553
 let accountEmail = "rakesh@google.com"
 var accoundPassword = "123456"
 accountCity = "Jaipur"
+let accountState;
 
-accountId = 2
+// accountId = 2
+// not allowed const can't be reassigned
+
+accountEmail = "rd@rd.com"
+accoundPassword = "2121212"
+accountCity = "Bengaluru"
 
 console.log(accountId);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail, accoundPassword, accountCity, accountState])
